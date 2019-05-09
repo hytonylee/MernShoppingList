ab651ee38e11c07078b74d1ff0fce1f34766067b
Tony Lee <hy.tony.lee@gmail.com>
2019-05-09 13:45:20 -0700

    Add AppNavBar'

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavBar from './components/AppNavBar'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavBar />
+      </div>
+    )
+  }
 }
 
 export default App;
